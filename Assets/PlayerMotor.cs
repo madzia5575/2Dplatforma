b5bec8de18ba2db8821dae09f5584796651ed9cb
madzia5575 <dsw55881@student.dsw.edu.pl>
2026-03-18 10:41:05 +0100

    Advanced player movements

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -4,22 +4,66 @@ using UnityEngine.InputSystem;
 public class PlayerMotor : MonoBehaviour
 {
     Vector2 direction;
+    Rigidbody2D Rigidbody2D;
     public float speed = 10;
+    public float jumpForce = 10;
+    private bool canJump = true;
+    public float maxSpeed = 5;
+    public float stoppingForce = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    private void Update()
+    // Update is called once per frame (not anymore :)))
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        //transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        HandlePlayerMovement();
+        MaxSpeedLimiting();
+    }
 
+    private void MaxSpeedLimiting()
+    {
+        if (Rigidbody2D.linearVelocityX >= maxSpeed)
+        {
+            Rigidbody2D.linearVelocityX = maxSpeed;
+        }
+        else if (Rigidbody2D.linearVelocityX <= -maxSpeed)
+        {
+            Rigidbody2D.linearVelocityX = -maxSpeed;
+        }
+    }
+
+    private void HandlePlayerMovement()
+    {
+        if (direction.x != 0)
+        {
+            Rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
+        }
+        else if (direction.x != 0)
+        {
+            Rigidbody2D.AddForce(new Vector2(-Rigidbody2D.linearVelocityX * stoppingForce, 0));
+        }
     }
 
     private void OnMove(InputValue value)
     {
         direction = value.Get<Vector2>();
+    }
+
+    private void OnJump()
+    {
+        if(canJump)
+        {
+            Rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
     }
 }
